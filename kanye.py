4ccfe863fec7c1a2048be441d74f22d0ffde8700
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-12-22 20:35:24 -0500

    added code to make Api request

--- kanye.py
+++ kanye.py
@@ -3,20 +3,26 @@ import requests
 
 
 def get_quote():
-    pass
+    response = requests.get(url="https://api.kanye.rest/")
+    response.raise_for_status()
+    data = response.json()
+    quote = data['quote']
+    bg_canvas.itemconfig(quote_text,text=quote, fill="white")
 
 
 
 window = Tk()
 window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, )
 
 kanye_img = PhotoImage(file="kanye.png")
 background_img = PhotoImage(file="background.png")
 bg_canvas = Canvas(width=350, height=416)
 bg_canvas.create_image(175,208, image=background_img)
-bg_canvas.create_text(175,208, text="Belive in your flyness...conquer your shyness",font=("Arial", 32, "normal"))
+quote_text = bg_canvas.create_text(175,208,width=250,font=("Arial", 25, "bold"))
 bg_canvas.grid(column=0, row=0)
+
+get_quote()
 
 
 kanye_button = Button(image=kanye_img,highlightthickness=0, command=get_quote)
