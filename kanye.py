5507fee667ab307299b580907525c6719e05ad1c
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-12-22 19:29:48 -0500

    starter files added

---
+++ kanye.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=10, pady=10)
+
+
+
+background_img = PhotoImage(file="background.png")
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
